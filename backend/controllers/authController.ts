674092a998620c22af45d5ce2d04a3404dfd65c6
alimirhashimli1 <alimirhashimli@gmail.com>
2025-06-18 01:42:15 +0200

    added login controller

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -52,3 +52,37 @@ export const registerUser = async (req: Request, res: Response) => {
         res.status(500).json({message: 'Registration failed'})
     }
 }
+
+export const loginUser = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    try {
+        const user = await prisma.user.findUnique({ where : { email }});
+        if(!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if(!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+            
+        }
+
+        const token = jwt.sign( {id: user.id, isAdmin: user.isAdmin}, process.env.JWT_SECRET as string, { expiresIn: '1d'} );
+
+        res.status(200).json({
+            message: 'Login successful',
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                fullname: user.fullname,
+                isAdmin: user.idAdmin
+            }
+        })
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Login failed'})
+    }
+}
